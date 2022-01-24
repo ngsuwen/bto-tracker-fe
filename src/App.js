9392b35ed8b8bf6fd56aded660bceef69d1b6982
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2022-01-24 16:16:19 +0800

    added cover bg

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 
 // pages
 import ScrollableTabs from "./components/general/tabs";
+import CoverPic from "./components/homepage/cover";
 
 function App() {
   return (
     <>
       <ScrollableTabs />
+      <CoverPic/>
     </>
   );
 }
---
+++ src/components/homepage/cover.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { Box } from "@mui/material";
+
+export default function CoverPic() {
+  return (
+    <Box
+    className="cover-bg" style={{"--img": "url('https://sg.tepcdn.com/public/usr/dsc14e/f46afb-macpherson-weave.jpg')"}}>
+      <h1>some text here</h1>
+    </Box>
+  );
+}
