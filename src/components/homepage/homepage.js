09a0c2e57ab530014794ba2f7ce61cbecfe709f6
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2022-01-26 23:35:47 +0800

    added react-router-hash-link

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -7,12 +7,15 @@ import { Container, Typography, Grid, Button, Box } from "@mui/material";
 // home components
 import CoverPic from "./cover";
 import ProjectCard from "./projectCard";
-import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 // breakpoint
 const topOffset = (theme) => ({
   [theme.breakpoints.down(500)]: { marginBottom: "8vh", marginTop: "180px" },
-  [theme.breakpoints.up(500)]: { marginBottom: "8vh", marginTop: "calc(8vh + 100px)" }
+  [theme.breakpoints.up(500)]: {
+    marginBottom: "8vh",
+    marginTop: "calc(8vh + 100px)",
+  },
 });
 
 function Home() {
@@ -23,7 +26,11 @@ function Home() {
         <Typography variant="h5" marginBottom="3vh">
           Ongoing Projects
         </Typography>
-        <Grid container spacing={2} sx={{display:"flex",justifyContent:"center"}}>
+        <Grid
+          container
+          spacing={2}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
           <Grid item lg={3}>
             <ProjectCard />
           </Grid>
@@ -49,17 +56,32 @@ function Home() {
             <ProjectCard />
           </Grid>
         </Grid>
-        <Box sx={{width:"100%", display:"flex",justifyContent:"center", marginTop:"3vh"}}>
-        <Link style={{ textDecoration: 'none' }} to={{pathname:"/projects", hash:"#ongoing"}}>
-          <Button variant="outlined">View More</Button>
-          </Link>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "3vh",
+          }}
+        >
+          <HashLink
+            style={{ textDecoration: "none" }}
+            scroll={(el) => el.scrollIntoView({ behavior: 'smooth' })}
+            to={"/projects#ongoing"}
+          >
+            <Button variant="outlined">View More</Button>
+          </HashLink>
         </Box>
       </Container>
       <Container maxWidth="xl" sx={{ marginY: "8vh" }}>
         <Typography variant="h5" marginBottom="3vh">
           Upcoming Projects
         </Typography>
-        <Grid container spacing={2} sx={{display:"flex",justifyContent:"center"}}>
+        <Grid
+          container
+          spacing={2}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
           <Grid item lg={3}>
             <ProjectCard />
           </Grid>
@@ -73,10 +95,21 @@ function Home() {
             <ProjectCard />
           </Grid>
         </Grid>
-        <Box sx={{width:"100%", display:"flex",justifyContent:"center", marginTop:"3vh"}}>
-          <Link style={{ textDecoration: 'none' }} to={{pathname:"/projects", hash:"#upcoming"}}>
-          <Button variant="outlined">View More</Button>
-          </Link>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "3vh",
+          }}
+        >
+          <HashLink
+            style={{ textDecoration: "none" }}
+            scroll={(el) => el.scrollIntoView({ behavior: 'smooth' })}
+            to={"/projects#upcoming"}
+          >
+            <Button variant="outlined">View More</Button>
+          </HashLink>
         </Box>
       </Container>
     </>
--- src/components/infopage/projectListPage.js
+++ src/components/infopage/projectListPage.js
@@ -8,86 +8,98 @@ import ProjectCard from "../homepage/projectCard";
 function ProjectList() {
   return (
     <>
-      <Container maxWidth="xl" sx={{marginBottom: "8vh", marginTop: "8vh"}}>
-        <Typography id="ongoing" variant="h5" marginBottom="3vh">
-          Ongoing Projects
-        </Typography>
-        <Grid container spacing={2} sx={{display:"flex",justifyContent:"center"}}>
-          <Grid item lg={3}>
-            <ProjectCard />
+      <section id="ongoing">
+        <Container maxWidth="xl" sx={{ marginBottom: "8vh", marginTop: "8vh" }}>
+          <Typography variant="h5" marginBottom="3vh">
+            Ongoing Projects
+          </Typography>
+          <Grid
+            container
+            spacing={2}
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
           </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
+        </Container>
+      </section>
+      <section id="upcoming">
+        <Container maxWidth="xl" sx={{ marginY: "8vh" }}>
+          <Typography variant="h5" marginBottom="3vh">
+            Upcoming Projects
+          </Typography>
+          <Grid
+            container
+            spacing={2}
+            sx={{ display: "flex", justifyContent: "center" }}
+          >
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item lg={3}>
+              <ProjectCard />
+            </Grid>
           </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-        </Grid>
-      </Container>
-      <Container maxWidth="xl" sx={{ marginY: "8vh" }}>
-        <Typography id="upcoming" variant="h5" marginBottom="3vh">
-          Upcoming Projects
-        </Typography>
-        <Grid container spacing={2} sx={{display:"flex",justifyContent:"center"}}>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item lg={3}>
-            <ProjectCard />
-          </Grid>
-        </Grid>  
-      </Container>
+        </Container>
+      </section>
     </>
   );
 }
