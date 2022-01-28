4d79f7d204357a9f050299891c2b8335d8e72c26
LAPTOP-KH9KVP83\Suwen <ng.suwen@dhs.sg>
2022-01-28 10:30:38 +0800

    added menu list to tabs contribution

--- src/components/general/tabs.js
+++ src/components/general/tabs.js
@@ -1,10 +1,18 @@
 import * as React from "react";
-import { Container, Box, Tab, Tabs, styled } from "@mui/material";
+import {
+  Container,
+  Box,
+  Tab,
+  Tabs,
+  styled,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 
 // custom breakpoint
 const nav = (theme) => ({
-  [theme.breakpoints.down('md')]: {display:'none'},
-  [theme.breakpoints.up('md')]: {bgcolor: "#333333", display:'block'}
+  [theme.breakpoints.down("md")]: { display: "none" },
+  [theme.breakpoints.up("md")]: { bgcolor: "#333333", display: "block" },
 });
 
 const StyledTabs = styled((props) => (
@@ -23,34 +31,47 @@ const StyledTabs = styled((props) => (
   },
 });
 
-const StyledTab = styled((props) => <a id="tab-nav" href={`#${props.link}`}><Tab disableRipple {...props} /></a>)(
-  ({ theme }) => ({
-    textTransform: "none",
-    fontWeight: theme.typography.fontWeightBold,
-    fontSize: theme.typography.pxToRem(14),
-    marginRight: theme.spacing(1),
+const StyledTab = styled((props) => (
+  <a id="tab-nav" href={`#${props.link}`}>
+    <Tab disableRipple {...props} />
+  </a>
+))(({ theme }) => ({
+  textTransform: "none",
+  fontWeight: theme.typography.fontWeightBold,
+  fontSize: theme.typography.pxToRem(14),
+  marginRight: theme.spacing(1),
+  color: "inherit",
+  "&.Mui-selected": {
     color: "inherit",
-    "&.Mui-selected": {
-      color: "inherit",
-    },
-  })
-);
+  },
+}));
 
-const StyledTab2 = styled((props) => <a id={props.specialId} href={`#${props.link}`}><Tab disableRipple {...props} /></a>)(
-  ({ theme }) => ({
-    textTransform: "none",
-    fontWeight: theme.typography.fontWeightBold,
-    fontSize: theme.typography.pxToRem(14),
-    marginRight: theme.spacing(1),
+const StyledTab2 = styled((props) => (
+  <a id={props.specialId} href={`#${props.link}`}>
+    <Tab disableRipple {...props} />
+  </a>
+))(({ theme }) => ({
+  textTransform: "none",
+  fontWeight: theme.typography.fontWeightBold,
+  fontSize: theme.typography.pxToRem(14),
+  marginRight: theme.spacing(1),
+  color: "inherit",
+  "&.Mui-selected": {
     color: "inherit",
-    "&.Mui-selected": {
-      color: "inherit",
-    },
-  })
-);
+  },
+}));
 
 export default function CustomizedTabs() {
   const [value, setValue] = React.useState(0);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -71,20 +92,38 @@ export default function CustomizedTabs() {
               onChange={handleChange}
               aria-label="styled tabs example"
             >
-              <StyledTab label="Home" link="/"/>
-              <StyledTab label="BTO Projects" link="/projects"/>
-              <StyledTab label="Watchlist" link="/tracker/projects"/>
-              <StyledTab label="Useful links" link="/links"/>
-              <StyledTab label="FAQs" link="/faq"/>
-              <StyledTab label="Disclaimer" link="/disclaimer"/>
+              <StyledTab label="Home" link="/" />
+              <StyledTab label="BTO Projects" link="/projects" />
+              <StyledTab label="Watchlist" link="/tracker/projects" />
+              <StyledTab label="Useful links" link="/links" />
+              <StyledTab label="FAQs" link="/faq" />
+              <StyledTab label="Disclaimer" link="/disclaimer" />
             </StyledTabs>
           </Box>
           <Box>
             <StyledTabs value={1} aria-label="styled tabs example">
-              <StyledTab2 label="+ Contribute" specialId="contribute-tab"/>
-              <StyledTab2 label="Sign In" specialId="tab-nav"/>
+              <StyledTab2
+                onClick={handleClick}
+                label="+ Contribute"
+                specialId="contribute-tab"
+              />
+              <StyledTab2 label="Sign In" specialId="tab-nav" />
             </StyledTabs>
           </Box>
+          <Menu
+            id="contribute-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>Data Scrapper</MenuItem>
+            <MenuItem onClick={handleClose}>Admin</MenuItem>
+            <MenuItem onClick={handleClose}>Appointment Date</MenuItem>
+            <MenuItem onClick={handleClose}>Feedback</MenuItem>
+          </Menu>
         </Box>
       </Container>
     </Box>
